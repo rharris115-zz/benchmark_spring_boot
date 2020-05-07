667c2e44fb0502e5f190c5fa0515b2d275c22ce3
rich <richard.w.harris@mac.com>
2020-05-07 17:06:44 +0100

    Change to path variable.

--- src/main/java/org/example/rest/EstimatePiController.java
+++ src/main/java/org/example/rest/EstimatePiController.java
@@ -2,7 +2,7 @@ package org.example.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 import java.util.Map;
@@ -13,14 +13,14 @@ import java.util.stream.Stream;
 public class EstimatePiController {
 
 
-    @GetMapping("/estimate-pi")
-    public Map<String, Double> estimatePi(@RequestParam(name = "n") Integer n) {
+    @GetMapping("/estimate-pi/{n}")
+    public Map<String, Double> estimatePi(@PathVariable Integer n) {
 
 
         final Random rnd = new Random();
 
 
-        double piEstimate = 4d * Stream.generate(() -> {
+        final double piEstimate = 4d * Stream.generate(() -> {
             double x = rnd.nextDouble();
             double y = rnd.nextDouble();
             return (x * x + y * y) < 1 ? 1 : 0;
