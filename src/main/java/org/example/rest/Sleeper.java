4a08fcbeea4a0758ec7651dd956033c2051c34fd
rich <richard.w.harris@mac.com>
2020-05-08 16:03:12 +0100

    Make sleeper asynchronous.

--- src/main/java/org/example/rest/Sleeper.java
+++ src/main/java/org/example/rest/Sleeper.java
@@ -1,6 +1,5 @@
 package org.example.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
