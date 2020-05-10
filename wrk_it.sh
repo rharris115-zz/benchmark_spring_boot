de9af8f7dbcad431c1dda59f2c78df95428cb4c2
rich <richard.w.harris@mac.com>
2020-05-10 22:55:18 +0100

    Update wrk_it.sh. Re-do data in README.md.

--- wrk_it.sh
+++ wrk_it.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-wrk_path=$1
-host=$2
-t=$3
-d=$4
-c=$5
+t=$1
+c=$2
+d=$3
+host=$4
 
-end_points=(/hello
+end_points=(
+  /hello
   /sleep/0.1 /sleep/0.5 /sleep/1.0
   /estimate-pi/100 /estimate-pi/1000 /estimate-pi/10000 /estimate-pi/100000 /estimate-pi/1000000
-  /estimate-pi-np/100 /estimate-pi-np/1000 /estimate-pi-np/10000 /estimate-pi-np/100000 /estimate-pi-np/1000000)
+  /estimate-pi-np/100 /estimate-pi-np/1000 /estimate-pi-np/10000 /estimate-pi-np/100000 /estimate-pi-np/1000000
+)
 
 for end_point in "${end_points[@]}"; do
-  $wrk_path/wrk/wrk -t$t -c$c -d$d http://$host$end_point
+
+  url=http://$host$end_point
+
+  echo 'Path:'
+  echo $end_point
+  echo ''
+  echo 'Example Output:'
+  echo 'curl '$url
+  curl $url
+  echo ''
+  echo 'Wrk Report:'
+  wrk -t$t -c$c -d$d $url
+  echo ''
   echo ''
   sleep 10
 done
